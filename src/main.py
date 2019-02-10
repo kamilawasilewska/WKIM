541f679e9bc68f3c921273ac82f23747dce37607
wopoczynski <wojtek.opoczynski@gmail.com>
2019-02-10 21:50:53 +0100

    console running

--- src/main.py
+++ src/main.py
@@ -1,31 +1,45 @@
 from src.Service.ManageImage import ManageImage
 import config
+import sys
+
+a = sys.argv
+if len(sys.argv) > 1:
+    transformation = str(sys.argv[1])
+else:
+    transformation = 'BSpline'
 
 manage = ManageImage()
 
 imageFixedPath = manage.getImagePath(config.IMAGES[0])
 imageMovingPath = manage.getImagePath(config.IMAGES[1])
-# reading imgs
+
 imageFixed = manage.readImage(imageFixedPath)
 imageMoving = manage.readImage(imageMovingPath)
 
-# processing
-resTranslation = manage.registrationMethodTranslation(imageFixed, imageMoving)
-# resTranslation2 = manage.registrationMethodTranslation2(imageFixed, imageMoving)
-# resTranslation3 = manage.registrationMethodTranslationWithSampling(imageFixed, imageMoving)
-# resTranslationCentered = manage.registrationMethodCenteredTransform(imageFixed, imageMoving) #trwa najdluzej
-# resBSpline = manage.registrationMethodBSpline(imageFixed, imageMoving)
-# resBSpline2 = manage.registrationMethodBSpline2(imageFixed, imageMoving)
-# resBSpline3 = manage.registrationMethodBSpline3(imageFixed, imageMoving)
-# resExhaustive = manage.registrationMethodExhaustive(imageFixed, imageMoving)
-
-# displaying required imageJ
-manage.displayResult(imageFixed, imageMoving, resTranslation)
-# manage.displayResult(imageFixed, imageMoving, resTranslation2)
-# manage.displayResult(imageFixed, imageMoving, resTranslation3)
-# manage.displayResult(imageFixed, imageMoving, resTranslationCentered)
-# manage.displayResult(imageFixed, imageMoving, resBSpline)
-# manage.displayResult(imageFixed, imageMoving, resBSpline2)
-# manage.displayResult(imageFixed, imageMoving, resBSpline3)
-# manage.displayResult(imageFixed, imageMoving, resExhaustive)
-
+# processing, displaying required imageJ in Users/User/Fiji.app otherwise will throw error
+if transformation == "translation":
+    resTranslation = manage.registrationMethodTranslation(imageFixed, imageMoving)
+    manage.displayResult(imageFixed, imageMoving, resTranslation)
+elif transformation == 'translation2':
+    resTranslation2 = manage.registrationMethodTranslation2(imageFixed, imageMoving)
+    manage.displayResult(imageFixed, imageMoving, resTranslation2)
+elif transformation == 'translation+sampling':
+    resTranslation3 = manage.registrationMethodTranslationWithSampling(imageFixed, imageMoving)
+    manage.displayResult(imageFixed, imageMoving, resTranslation3)
+elif transformation == 'centered':
+    resTranslationCentered = manage.registrationMethodCenteredTransform(imageFixed, imageMoving) #trwa najdluzej
+    manage.displayResult(imageFixed, imageMoving, resTranslationCentered)
+elif transformation == 'Exhaustive':
+    resExhaustive = manage.registrationMethodExhaustive(imageFixed, imageMoving)
+    manage.displayResult(imageFixed, imageMoving, resExhaustive)
+elif transformation == 'BSpline':
+    resBSpline = manage.registrationMethodBSpline(imageFixed, imageMoving)
+    manage.displayResult(imageFixed, imageMoving, resBSpline)
+elif transformation == 'BSpline2':
+    resBSpline2 = manage.registrationMethodBSpline2(imageFixed, imageMoving)
+    manage.displayResult(imageFixed, imageMoving, resBSpline2)
+elif transformation == 'BSpline3':
+    resBSpline3 = manage.registrationMethodBSpline3(imageFixed, imageMoving)
+    manage.displayResult(imageFixed, imageMoving, resBSpline3)
+else:
+    print('unsupported!')
